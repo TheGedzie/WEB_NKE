6b78d18db2d6d03a4b2d678ad83284eed9f4dafe
TheGedzie <gedzie06@gmail.com>
2025-03-29 22:05:46 +0300

    Objects in JS

---
+++ JS/ObjectsInJS/main.js
@@ -0,0 +1,65 @@
+// //1
+// let days = {
+//     1:"Monday",
+//     2:"Tuesday",
+//     3:"Wednesday",
+//     4:"Thursday",
+//     5:"Friday",
+//     6:"Saturday",
+//     7:"Sunday"
+// };
+// for (let i = 0; i <=7; i++) {
+//     console.log(days[i]);
+// }
+
+//2
+
+// const date = {
+//     year:"2025",
+//     month:"03",
+//     day:"29",
+// }
+// alert(date.year + "-" + date.month + "-" + date.day);
+
+//3
+
+// const obj = {x: 1, y: 2, z: 3};
+//
+// const values = Object.values(obj);
+// value = values.map(function (num){
+//     console.log(num * 2);
+// })
+
+
+//4
+
+// const people = {
+//     'name':"Ivan",
+//     'surname':"Ivanov",
+//     'patronymic':"Ivanovich",
+// }
+// alert(people.name + " " + people.surname + " " + people.patronymic);
+
+//5
+
+// let obj = {x: 1, y: 2, z: 3};
+// console.log(obj['x']);
+
+//6
+
+// let obj = {x: 1, y: 2, z: 3};
+// let key = obj.x;
+// console.log(key);
+
+//7
+//
+// let obj = {x: 1, y: 2, z: 3};
+// let sum = obj.x + obj.y + obj.z;
+// console.log(sum);
+
+
+//8
+//
+// let obj = {x: 1, y: 2, z: 3};
+// let keys = Object.keys(obj);
+// console.log(keys.length);
